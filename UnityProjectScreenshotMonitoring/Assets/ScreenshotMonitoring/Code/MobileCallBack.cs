f62c896be8ed67bece1bb3c3595f4455128779f7
WangHeMing <guiqi4danding@163.com>
2021-12-17 14:20:54 +0800

    feat:添加Unity演示项目

---
+++ UnityProjectScreenshotMonitoring/Assets/ScreenshotMonitoring/Code/MobileCallBack.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class MobileCallBack : MonoBehaviour
+{
+
+#if UNITY_IOS
+
+    [DllImport("__Internal")]
+    private static extern void InitScreenCaptureMonitoring();
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Debug.Log("InitScreenCaptureMonitoringForIOS");
+        InitScreenCaptureMonitoring();
+    }
+    
+    private void Callbake_iosMessage(string message)
+    {
+        AddCube();
+    }
+
+
+#elif UNITY_ANDROID
+
+
+
+#else
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            AddCube();
+        }
+    }
+
+#endif
+
+    private void AddCube()
+    {
+        var ins = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        ins.transform.position = Vector3.zero;
+        ins.AddComponent<Rigidbody>();
+
+        Destroy(ins, 5);
+    }
+
+
+}
---
+++ UnityProjectScreenshotMonitoring/Assets/ScreenshotMonitoring/Plugin/IOS/NativeCallPlugin.mm
@@ -0,0 +1,59 @@
+//
+//  NativeCall.m
+//  UnityFramework
+//
+//  Created by Jack Hu on 2021/12/15.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NativeCallPlugin : NSObject
+{
+    
+}
+@end
+
+@implementation NativeCallPlugin
+
+static NativeCallPlugin * _instance;
+
++(NativeCallPlugin*) Instance
+{
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        NSLog(@"Crating NativeCallPlugin Instance");
+        _instance =[[NativeCallPlugin alloc] init];
+    });
+    return _instance;
+}
+
+-(void)InstanceShooting
+{
+    NSOperationQueue *mainQueue = [NSOperationQueue mainQueue];
+    [[NSNotificationCenter defaultCenter] addObserverForName:UIApplicationUserDidTakeScreenshotNotification
+                                                      object:nil
+                                                       queue:mainQueue
+                                                  usingBlock:^(NSNotification *note) {
+        BackToUnityScene:nil;
+        NSLog("Shoot");
+    }];
+}
+
+-(void)BackToUnityScene:(id)sender{
+    UnitySendMessage("PluginCallBack", "Callbake_iosMessage", "Shoot");
+}
+
+@end
+
+
+
+extern "C"
+{
+    //初始化截图监测
+    void InitScreenCaptureMonitoring()
+    {
+        [[NativeCallPlugin Instance]InstanceShooting];
+    }
+    
+    
+}
